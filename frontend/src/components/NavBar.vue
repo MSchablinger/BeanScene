00e732187c5e005bea012a1c161c2eab39924a08
MSchablinger <mathias.schablinger@gmail.com>
2023-03-21 22:50:55 +0100

    Began Programming

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -1,9 +1,10 @@
 <template>
   <nav>
     <img src="../assets/Logo.png" alt="Logo">
-    Bean Scene
-    <a href=""></a>
-    
+    <h1>Bean Scene</h1>
+    <a href="">Shop</a>
+    <a href="">Merch</a>
+    <a href=""><img src="../assets/shopping_cart.svg" alt="basket"></a>
   </nav>
 </template>
 <script>
@@ -15,9 +16,16 @@ export default {
 nav {
   display: flex;
   height: 88px;
+  background-color: #45170C;
 }
 img {
   height: 100%;
   width: auto;
+}
+h1 {
+  font-family: "Edwardian Script ITC";
+  font-size: 70px;
+  margin: 0;
+  color:#ffffff;
 }
 </style>
