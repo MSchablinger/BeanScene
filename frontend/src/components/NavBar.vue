89fa569556fa94d6a7f150aef7bc57b90f23663f
MSchablinger <mathias.schablinger@gmail.com>
2023-03-21 22:35:21 +0100

    Setup Project

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <nav>
+    <img src="../assets/Logo.png" alt="Logo">
+    Bean Scene
+    <a href=""></a>
+    
+  </nav>
+</template>
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+<style>
+nav {
+  display: flex;
+  height: 88px;
+}
+img {
+  height: 100%;
+  width: auto;
+}
+</style>
